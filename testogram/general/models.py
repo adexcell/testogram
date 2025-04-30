585b5fb9edb69aea88f7df8bd1fcdcd0accadb31
[abakar.aliev] <[salachali@gmail.com]>
2025-04-30 17:37:41 +0500

    add __str__ to model Post

--- testogram/general/models.py
+++ testogram/general/models.py
@@ -21,6 +21,9 @@ class Post (models.Model):
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     
+    def __str__(self):
+        return f"({self.id}) {self.title}"
+    
 
 class Comment(models.Model):
     body = models.TextField()
